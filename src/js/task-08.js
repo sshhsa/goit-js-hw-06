e7b6a5802b1330cdc0b7bd4e4607055fec4b1fa9
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-03-26 14:51:47 +0300

    Commit

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -3,26 +3,41 @@ const form = document.querySelector('form.login_form');
 const inputEmail = document.querySelector('.input_email');
 const inputPassword = document.querySelector('.input_password');
 
+const buttonSubmit = document.getElementById('form_button');
+
 form.addEventListener('submit', onClickButtonForm);
 
 function onClickButtonForm(event) {
   event.preventDefault();
 
-  if (inputEmail.value === '' && inputPassword.value === '') {
+  if (inputEmail.value === '' || inputPassword.value === '') {
     alert('WARNING!!! All brackets must be with value');
   }
 
-  //   const formElements = event.currentTarget.elements;
-  //   const email = formElements.email.value;
-  //   const passsword = formElements.passsword.value;
-  //   const formData = { email, passsword };
-
-  //   console.log(formData);
-
-  const formData = new formData(event.currentTarget);
+  const formData = new FormData(event.currentTarget);
 
   formData.forEach((value, name) => {
     console.log('onFormSubmit -> name', name);
     console.log('onFormSubmit -> value', value);
   });
+
+  //   const formElements = event.currentTarget.elements;
+
+  //   console.log(formElements);
+
+  //   const mail = formElements.mail.value;
+  //   const password = formElements.password.value;
+
+  //   const formData = {
+  //     mail,
+  //     password,
+  //   };
+
+  //   console.log(formData);
+
+  buttonSubmit.addEventListener('click', onResetForm);
+
+  function onResetForm() {
+    form.reset();
+  }
 }
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,16 @@
+const buttonChangeColor = document.querySelector('.change_color');
+const colorValue = document.querySelector('.color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+buttonChangeColor.addEventListener('click', onButtonChangeColor);
+
+function onButtonChangeColor() {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorValue.textContent = newColor;
 }
